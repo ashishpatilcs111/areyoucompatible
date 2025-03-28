8f698396dcc02b33df004302940fca463c6c8e75
ashishpatilcs111 <ashishpatilcs111@gmail.com>
2025-03-28 09:52:29 +0530

    Initial commit: Node.js and React setup

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+// Serve the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
